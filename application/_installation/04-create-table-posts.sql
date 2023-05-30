a2f4101b7c816058b60ebdd7f55f51dfe73ff8f5
Dylan Renwick <skidsirongenesisdev@gmail.com>
2023-05-30 02:14:32 +0000

    Cleaned up and updated SQL install

--- application/_installation/04-create-table-posts.sql
+++ application/_installation/04-create-table-posts.sql
@@ -1,10 +1,15 @@
 CREATE TABLE IF NOT EXISTS `huge`.`posts` (
- `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
- `title` text NOT NULL,
- `text` text NOT NULL,
- `tags` text DEFAULT '',
- `categories` text DEFAULT '',
- `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
- `modified_at` datetime DEFAULT CURRENT_TIMESTAMP,
- PRIMARY KEY (`id`)
+	`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+	`user_id` int(11) unsigned NOT NULL,
+	`title` text NOT NULL,
+	`text` text NOT NULL,
+	`tags` text DEFAULT '',
+	`categories` text DEFAULT '',
+	`active` bit DEFAULT 0,
+	`created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+	`modified_at` datetime DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`),
+	CONSTRAINT `fk_post_author`
+		FOREIGN KEY (user_id) REFERENCES users (id)
+		ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
