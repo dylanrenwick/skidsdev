e894b77f54ea8a42413929a895a13e2850c2c16b
Dylan Renwick <skidsirongenesisdev@gmail.com>
2023-06-03 21:03:07 +0000

    Added series table creation script

---
+++ application/_installation/06-create-table-series.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `huge`.`series` (
+	`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+	`name` text NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
