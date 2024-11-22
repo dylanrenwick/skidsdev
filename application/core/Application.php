f298c3234a78fde82bf6aaa4ef8bf24f83292390
drenwick <skidsirongenesisdev@gmail.com>
2024-11-22 10:26:10 -0500

    Cleaned up Application class

--- application/core/Application.php
+++ application/core/Application.php
@@ -10,15 +10,6 @@ class Application
     /** @var mixed Instance of the controller */
     private Controller $controller;
 
-    /** @var array URL parameters, will be passed to used controller-method */
-    private array $parameters = array();
-
-    /** @var string Just the name of the controller, useful for checks inside the view ("where am I ?") */
-    private string $controller_name;
-
-    /** @var string Just the name of the controller's method, useful for checks inside the view ("where am I ?") */
-    private string $action_name;
-
     /**
      * Start the application, analyze URL elements, call according controller/method or relocate to fallback location
      */
@@ -34,7 +25,7 @@ class Application
             $this->getErrorController()->error404();
 
         // load this file and create this controller
-        // example: if controller would be "car", then this line would translate into: $this->car = new car();
+        // example: if controller would be "car", then this line would translate into: $this->controller = new car();
         require $controller_path;
         $this->controller = new $req->controller();
 
@@ -49,7 +40,7 @@ class Application
             // call the method and pass arguments to it
             call_user_func_array(array($this->controller, $req->action), $requestRoute);
         } else {
-            // if no parameters are given, just call the method without parameters, like $this->index->index();
+            // if no parameters are given, just call the method without parameters, like $this->controller->index();
             $this->controller->{$req->action}();
         }
     }
