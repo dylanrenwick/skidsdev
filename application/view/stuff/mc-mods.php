9bae7dc64e71b925b1ca2c5162fc5bcf54efd385
Dylan Renwick <skidsirongenesisdev@gmail.com>
2025-02-26 14:28:27 -0500

    Updated MC mods guide

--- application/view/stuff/mc-mods.php
+++ application/view/stuff/mc-mods.php
@@ -5,8 +5,8 @@
 <p>Now, simply create a new instance with the "Add Instance" button. Prism instances make it easy to maintain multiple installations of Minecraft with different mods and even game versions. Hello LondON currently uses:<pre>Minecraft v1.21.4</pre></p>
 <p>When you click Add Instance, you'll be presented with this screen:</p>
 <img src="https://i.imgur.com/HjTvIYT.png" style="max-width:50%;" alt="Prism Add Instance" /> <br/>
-<p>In addition to selecting the right game version, make sure to also select your desired Mod Loader in the bottom right corner. If you're unsure, I strongly recommend Fabric for playing on vanilla servers.</p>
-<p>Once you select a Mod Loader, it will load a list of versions for the loader. You can safely take whichever one has the gold star (usually the top one). Then, click OK.</p>
+<p>In addition to selecting the right game version, make sure to also select your desired Mod Loader in the bottom right corner.<br/>If you're unsure, I strongly recommend Fabric for playing on vanilla servers.</p>
+<p>Once you select a Mod Loader, it will load a list of versions for the loader. You can safely take the recommended version (the one with the gold star, usually the top one).<br/>Then, click OK.</p>
 <br/>
 <p>Now, select your new instance, and click "Edit" on the right of Prism. This will open the instance settings seen below.</p>
 <img src="https://i.imgur.com/gjg3CX2.png" style="max-width:50%;" alt="Prism Instance Settings" /> <br/>
@@ -17,7 +17,7 @@
 <img src="https://i.imgur.com/nqxHmj6.png" style="max-width:50%;" alt="Prism Install Mods" />
 <p><i>See that Sodium is both bolded and underlined, indicating it is selected for install.</i></p> <br/>
 <p>Once you've selected all the mods you want to install, click "Review and confirm" in the bottom right corner. This will open a small dialog listing the exact mods that will be installed, including any dependencies of mods you've selected. Hit OK and Prism will download and install all the mods for you!</p>
-<p><i>Note: Some mods on CurseForge do not allow 3rd party downloads, preventing Prism from downloading them for you. In this case, Prism will provide you a list of all the blocked mods, along with links directly to the download pages of each mod. Simply click all the links and visit all the pages in your browser to download the mods. Prism will detect them in your downloads folder and automatically install them.</i></p>
+<p><i><b>Note:</b> Some mods on CurseForge do not allow 3rd party downloads, preventing Prism from downloading them for you. In this case, Prism will provide you a list of all the blocked mods, along with links directly to the download pages of each mod. Simply click all the links and visit all the pages in your browser to download the mods. Prism will detect them in your downloads folder and automatically install them.</i></p>
 <br/>
 <p>Once all the mods are installed, you can launch the instance to launch Minecraft with your mods. Enjoy!</p>
 <p>Additionally, you may notice that beneath the "Mods" tab in the Instance Settings, there are "Resource packs" and "Shader packs" tabs. These work exactly the same as the mods tab and make installing texture packs or shaders a breeze!</p>
@@ -26,24 +26,24 @@
 <p>Below I've compiled a list of a few "must have" mods that I strongly recommend <i>everyone</i> install, as well as a list of "nice to have" mods that are by no means necessary, but might enhance your experience.</p>
 <h3>Must Have Mods</h3>
 <ul>
-<li><a class="active-link" href="https://modrinth.com/mod/sodium">Sodium</a> - A performance-enhancing mod that can drastically improve your rendering performance and framerate.</li>
-<li><a class="active-link" href="https://modrinth.com/mod/lithium">Lithium</a> - A performance-enhancing mod that can drastically improve your world simulation performance.</li>
-<li><a class="active-link" href="https://modrinth.com/mod/entityculling">Entity Culling</a> - A performance-enhancing mod that can drastically improve performance when around lots of entities.</li>
-<li><a class="active-link" href="https://modrinth.com/mod/iris">Iris</a> - This mod enables shaders, and is required if you want to use shader packs.</li>
+<li><a class="active-link" href="https://modrinth.com/mod/iris">Iris</a> - This mod enables shaders, and is <b>required</b> if you want to use shader packs.</li>
+<li><a class="active-link" href="https://modrinth.com/mod/sodium">Sodium</a> - A performance-enhancing mod that drastically improves rendering performance and framerate.</li>
+<li><a class="active-link" href="https://modrinth.com/mod/lithium">Lithium</a> - A performance-enhancing mod that drastically improves world simulation performance.</li>
+<li><a class="active-link" href="https://modrinth.com/mod/entityculling">Entity Culling</a> - A performance-enhancing mod that improves performance when around lots of entities.</li>
 <li><a class="active-link" href="https://modrinth.com/mod/immediatelyfast">Immediately Fast</a> - A performance-enhancing mod that generally improves UI rendering performance.</li>
-<li><a class="active-link" href="https://modrinth.com/mod/scoreboard-overhaul">Scoreboard Overhaul</a> - A mod that improves the look of the scoreboard, but more importantly hides it when it hasn't changed in a while.</li>
-<li><a class="active-link" href="https://modrinth.com/mod/fast-ip-ping">Fast IP Ping</a> - A mod that improves the speed of pings to IP-only servers, making the server status load and connecting to the server faster.</li>
-<li><a class="active-link" href="https://modrinth.com/mod/durability-tooltip">Durability Tooltip</a> - A self-explanatory mod that adds durability to the tooltips of tools and armor.</li>
-<li><a class="active-link" href="https://modrinth.com/mod/controlify">Controlify</a> - A mod that adds full and seamless controller support to Java Edition Minecraft. A must-have for playing on Steam Deck.</li>
+<li><a class="active-link" href="https://modrinth.com/mod/scoreboard-overhaul">Scoreboard Overhaul</a> - Improves the look of the scoreboard, but more importantly hides it when it hasn't changed in a while.</li>
+<li><a class="active-link" href="https://modrinth.com/mod/fast-ip-ping">Fast IP Ping</a> - Improves the speed of pings to IP-only servers, making the server status load and connecting to the server faster.</li>
+<li><a class="active-link" href="https://modrinth.com/mod/durability-tooltip">Durability Tooltip</a> - Adds durability to the tooltips of tools and armor.</li>
+<li><a class="active-link" href="https://modrinth.com/mod/controlify">Controlify</a> - Adds full and seamless controller support to Java Edition Minecraft. A must-have for playing on Steam Deck.</li>
 </ul>
 <h3>Nice to Have Mods</h3>
 <ul>
-<li><a class="active-link" href="https://modrinth.com/mod/appleskin">AppleSkin</a> - A simple mod that shows saturation levels on your hunger bar, as well as how much hunger and health the food you're holding will restore.</a>
-<li><a class="active-link" href="https://modrinth.com/mod/clientsort">Client Sort</a> - A mod that provides client-side inventory sorting. Simply middle-mouse-click in any inventory to quickly sort the contents.</li>
-<li><a class="active-link" href="https://modrinth.com/mod/xaeros-minimap">Xaero's Minimap</a> - A minimap mod that shows a small map in the corner of your screen. There are many minimap mods. This is the best one.</li>
-<li><a class="active-link" href="https://modrinth.com/mod/xaeros-world-map">Xaero's World Map</a> - A world map mod that allows you to view a full-screen map of the world. This is a great mod for exploring and finding your way around. Will only show chunks you've personally explored.</li>
-<li><a class="active-link" href="https://modrinth.com/mod/jade">Jade</a> - A mod that adds a handy tooltip at the top of the screen to show what you're currently looking at, block or entity. Also shows the current health of mobs when looking at them.</li>
-<li><a class="active-link" href="https://modrinth.com/mod/mouse-tweaks">Mouse Tweaks</a> - A mod that adds a variety of UI-related mouse tweaks to the game.</li>
+<li><a class="active-link" href="https://modrinth.com/mod/appleskin">AppleSkin</a> - Shows saturation levels on your hunger bar, as well as how much hunger and health the food you're holding will restore.</a>
+<li><a class="active-link" href="https://modrinth.com/mod/clientsort">Client Sort</a> - Provides client-side inventory sorting. Simply middle-mouse-click in any inventory to quickly sort the contents.</li>
+<li><a class="active-link" href="https://modrinth.com/mod/xaeros-minimap">Xaero's Minimap</a> - Shows a small map in the corner of your screen. There are many minimap mods. This is the best one.</li>
+<li><a class="active-link" href="https://modrinth.com/mod/xaeros-world-map">Xaero's World Map</a> - Allows you to view a full-screen map of the world. Will only show chunks you've personally explored.</li>
+<li><a class="active-link" href="https://modrinth.com/mod/jade">Jade</a> - Displays info about what you're currently looking at, block or entity, on the HUD. Also shows the current health of mobs when looking at them.</li>
+<li><a class="active-link" href="https://modrinth.com/mod/mouse-tweaks">Mouse Tweaks</a> - Adds a variety of UI-related mouse tweaks to the game.</li>
 </ul>
 <br/>
 <p>If you have any questions reach out to me in the discord @Mayube!</p>
