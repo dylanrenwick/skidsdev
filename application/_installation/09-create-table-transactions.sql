9b441037a70cb4b68ad9095686b4bffa9bb8107a
Dylan Renwick <skidsirongenesisdev@gmail.com>
2025-05-02 18:15:53 -0400

    Fixed trailing comma

--- application/_installation/09-create-table-transactions.sql
+++ application/_installation/09-create-table-transactions.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS `huge`.`transactions` (
 	`date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`category_id` int(11) unsigned NOT NULL,
 	PRIMARY KEY (`id`),
-	FOREIGN KEY (`category_id`) REFERENCES `huge`.`transaction_categories` (`id`) ON UPDATE CASCADE,
+	FOREIGN KEY (`category_id`) REFERENCES `huge`.`transaction_categories` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
